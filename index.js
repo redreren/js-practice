68b8b5682de8378a50f3294551e2c2dafbd811ad
redreren <jelka0503@gmail.com>
2020-09-03 14:48:42 +0300

    added files from the first part of the day

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+// let b = 1;
+// // b = 2;
+// console.log(b);
+
+// const c = 2;
+// console.log(c);
+
+// -------------------------
+
+// let a = 1;
+// let b = 2;
+// // let c = a + b;
+// console.log(a + b);
+// alert("hello world");
+
+// --------------------------
+
+// let a = 1;
+// let b = "1";
+
+// let c = a + b;
+
+// console.log(c);
+
+// -----------------
+
+// let a = 1;
+// let b = 2;
+
+// console.log(a < b);
+// console.log("first: ", 1 < 2 < 3, "second: ", 3 > 2 > 1);
+// console.log((1.1 * 10 + 1.3 * 10) / 10);
