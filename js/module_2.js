4f51690042432acc8490a72e266ff6980a4d1e66
redreren <jelka0503@gmail.com>
2020-09-08 19:38:57 +0300

    module-2

--- js/module_2.js
+++ js/module_2.js
@@ -192,8 +192,161 @@
 // min(2, 5) == 2
 // min(3, -1) == -1
 // min(1, 1) == 1
+
 // #7
 // а теперь максимальное число если функция принимает неограниченое к-во аргументов
-// const max = function () {}
+// const max = function () {};
 
-// ----------- ПРАКТИКА -------------
+// const max = function (...args) {
+//   let result;
+//   result = Math.max(...args);
+//   console.log(result);
+
+//   return result;
+// };
+// max(3, 8, 14, -7, -22);
+
+// ---------- DAY 2 ---------------
+
+// const humanCreate = function (skin, foot, gender, jewelry) {
+//   return `Human with ${skin} skin and ${foot} feet size has been created`;
+// };
+
+// const humanCreate = (skin, foot, gender, jewelry) =>
+//   `Human with ${skin} skin and ${foot} feet size has been created`;
+
+// console.log(humanCreate("yellow", 7, "male", "none"));
+
+// #2
+// Напишите функцию hello(), которая при вызове будет принимать переменную name (например, «Василий») и
+// выводить строку (в нашем случае «Привет, Василий»).  В случае отсутствующего аргумента выводить «Привет, гость»
+
+// const greeting = function (name = "guest") {
+//   console.log(`Hello, ${name}`);
+// };
+
+// greeting("Alex");
+
+// #3
+// Создайте функцию repeat(str, n), которая возвращает строку, состоящую из
+//  n повторений строки str. n — по умолчанию 2, str — пустая строка
+
+// const repeat = function (str = "", n = 2) {
+//   let result = "";
+//   for (let i = 0; i < n; i++) {
+//     result += str;
+//   }
+//   console.log(result);
+// };
+// repeat("Whale", 7);
+
+// #4
+// здайте функцию avg() , которая будет находить среднее значение по всем своим аргументам
+// (аргументы величины числовые).
+
+// const avg = function (...args) {
+//   let total = 0;
+//   for (const argument of args) {
+//     console.log(argument);
+//     total += argument;
+//   }
+//   console.log(total);
+//   const result = total / args.length;
+//   console.log(result);
+// };
+
+// avg(5, 7, 2, 8, 9);
+
+// ПРАКТИКА
+
+// FUNCTION EXPRASSION
+
+// var toShowName = function (firstName, secondName) {
+//   console.log(`${firstName} ${secondName}`);
+// };
+// toShowName("Alfredo", "Viser");
+
+// ARROW FUNCTION
+
+// const toShowName = (firstName, secondName) =>
+//   console.log(`${firstName} ${secondName}`);
+
+// toShowName("Alfredo", "Viser");
+
+//  FUNCTION DECLARATION
+
+// toShowName("Alfredo", "Viser");
+
+// function toShowName(firstName, secondName) {
+//   console.log(`${firstName} ${secondName}`);
+// }
+
+// ===================== ADDITIONAL ========================== //
+// // == task-1 == //
+// Написати ф-ю capitalize яка буде приймати строку і буде вертати нову строку де кожне слово буде починатися з великої літери.
+
+// const capitalize = function (string) {
+//   // return string.toUpperCase();
+//   let word = string.split(" ");
+//   let arrCapitalize = [];
+//   //   console.log(word);
+//   for (let elem of word) {
+//     // console.log(typeof elem);
+//     let wordCapitilize = elem[0].toUpperCase() + elem.substring(1);
+//     // console.log(wordCapitilize);
+//     arrCapitalize.push(wordCapitilize);
+//   }
+//   return arrCapitalize.join(" ");
+// };
+// console.log(capitalize("the quick brown fox"));
+
+// capitalize('the quick brown fox') // 'The Quick Brown Fox '
+
+// // == task-2 == //
+// Написати ф-ю countVowels яка буде приймати строку і буде вертати кількість голосних літер. aeiouAEIOU - рядок з голосними в англ алфавіті
+
+// const vowels = "aeiouAEIOU";
+
+// const countVowels = function (string) {
+//   //   console.log(string);
+//   let arr = string.split("");
+//   //   console.log(arr);
+//   let a = [];
+//   for (let elem of arr) {
+//     // console.log(elem);
+//     if (vowels.includes(elem)) {
+//       //   console.log("ok", elem);
+//       a.push(elem);
+//       //   console.log(a);
+//     }
+//   }
+//   //   console.log(a);
+//   console.log(a.length);
+// };
+// countVowels("the quick brown fox");
+
+// countVowels('the quick brown fox') // 5
+
+// // == task-3 == //
+// Написати ф-ю яка приймає 2 параметра (розміри в ремах - "2rem) і велечину базового шрифта ("20px") і вертає конвертований розмір в пікселях
+
+// // == task-4 == //
+// Написати ф-ю що приймає число (температура в цельсіях) і виводить результат в фаренгейтах
+
+// // == task-5 == //
+// Написати ф-ю calculateDogAge яка приймає один параметр - вік собачки. Результатом її роботи буде вік перевединий на людський (1 рік собаки це 7 років в людини)
+
+// // == task-6 == //
+// Написати ф-ю rgbToHex яка приймає колір в форматі rgb і повертає колір в форматі hex
+
+// // == task-7 == //
+// Написати ф-ю яка приймає масив і видаляє звідти всі унікальні елементи
+// nonUniqueElements([1, 2, 3, 1, 3]) // [1, 3, 1, 3]
+// nonUniqueElements([1, 2, 3, 4, 5]) // []
+// nonUniqueElements([5, 5, 5, 5, 5]) // [5, 5, 5, 5, 5]
+// nonUniqueElements([10, 9, 10, 10, 9, 8]) // [10, 9, 10, 10, 9]
+
+// // == task-8 == //
+// Написати ф-ю median яка приймає масив і знаходить його медіану Медіана – це числове значення, яке ділить відсортований масив чисел на більшу і меншу половини. У відсортованому масиві з непарним числом елементів медіана – це число в середині масиву. Для масиву з парним числом елементів, де нема ні одного елемента точно посередині, медіана – це середнє значення двох чисел, які знаходяться в середині масиву. В цій задачі заданий непустий масив натуральних чисел. Вам потрібно знайти медіану даного масиву.
+// median([1, 2, 3, 4, 5]) // 3
+// median([3, 6, 10, 15, 20, 99]) // 12.5
