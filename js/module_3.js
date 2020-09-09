e2ff91d5656566e8658bea2ce43be405c351d37f
redreren <jelka0503@gmail.com>
2020-09-09 21:01:47 +0300

    module3 day 1

---
+++ js/module_3.js
@@ -0,0 +1,181 @@
+//!________OBJECTS
+
+// const testObj = {
+//   test: "123",
+// };
+// testObj.test = "test";
+// // delete testObj.test;
+// console.log(testObj["test"]);
+
+// let name = "Resort Hotel";
+// let stars = 5;
+
+// // Эта ES5 запись избыточна
+// const es5hotel = {
+//   name: name,
+//   stars: stars,
+//   capacity: 100,
+// };
+
+// /*
+//  * ES6 Shorthand properties
+//  * Имя ключа будет аналогично имени переменной
+//  * Значение свойства будет равно значению переменной
+//  */
+// const es6hotel = {
+//   name,
+//   stars,
+//   capacity: 100,
+// };
+
+// console.log(es6hotel); // {name: "Resort Hotel", stars: 5, capacity: 100}
+
+// const obj = {
+//   defaultValue: 0,
+//   add: function () {
+//     obj.defaultValue += 1;
+//     const a = 10;
+//     return a;
+//   },
+//   onDecrement: function () {
+//     obj.defaultValue -= 1;
+//   },
+// };
+
+// obj.add();
+// obj.add();
+// obj.add();
+// obj.add();
+// obj.add();
+// obj.onDecrement();
+
+// console.log(obj);
+
+// let increment = obj.add;
+// // console.log(increment);
+
+// increment();
+// obj.mul = function (n) {
+//   obj.defaultValue *= n;
+// };
+
+// obj.mul(3);
+
+// console.log(obj);
+
+// -----------------------------
+
+// const object = {
+//   defaultValue: 0,
+//   add: function (n) {
+//     object.defaultValue += n;
+//   },
+//   onDecrement: function (n) {
+//     object.defaultValue -= n;
+//   },
+//   multiply: function (n) {
+//     object.defaultValue *= n;
+//   },
+//   divide: function (n) {
+//     object.defaultValue /= n;
+//   },
+//   showMe: function () {
+//     console.log(`The final value is: ${object.defaultValue}`);
+//   },
+// };
+// object.add(5);
+// object.onDecrement(4);
+// object.multiply(6);
+// object.divide(2);
+// console.log(object);
+// object.showMe();
+
+// ------------------------
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+// let total = 0;
+
+// for (let key in obj) {
+//   console.log(`${key} is equal to ${obj[key]}`);
+//   total += obj[key];
+// }
+// console.log(total);
+
+// -----------------------
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+// console.log(Object.entries(obj)[1][1]);
+// console.log(Object.entries(obj)[1][2]);
+// console.log((Object.entries(obj)[1][2] = 4));
+// console.log(Object.entries(obj)[1][0]);
+// console.log(Object.entries(obj)[1]);
+
+// ------------SPREAD---------------
+
+// let obj1 = {
+//   a: 1,
+//   b: 2,
+// };
+
+// let obj2 = {
+//   b: 5,
+//   c: 7,
+// };
+
+// let obj3 = { ...obj1, obj2 };
+
+// console.log(obj3);
+
+// let obj4 = { ...obj1, ...obj2 };
+
+// console.log(obj4);
+
+// let obj5 = { q: 15, ...obj1, p: 20, ...obj2 };
+
+// console.log(obj5);
+
+// let r = 12;
+// let w = 18;
+// let obj6 = { w, ...obj1, r, ...obj2 };
+
+// console.log(obj6);
+
+// ------------REST-------------
+
+// const add = function (value, ...args) {
+//   console.log(value); // первый аргумент
+//   console.log(args); // массив всех остальных аргументов
+// };
+
+// add(10, 1, 2, 3);
+// add(15, 1, 2, 3, 4, 5);
+
+// ------------ДЕСТРУКТУРИЗАЦИЯ--------------
+
+// const hotel = {
+//   name: "Resort Hotel",
+//   stars: 5,
+//   capacity: 100,
+//   status: "semilux",
+// };
+
+// // console.log(name);
+
+// const { name = hotelName, stars = 0, capacity = 0, status = "vip" } = hotel;
+
+// // console.log(name, stars, capacity);
+// console.log(status);
+// console.log(hotel);
